3d6756e42450fc891f06b71a53c243d11954670a
frank junior <juniorngassam@gmail.com>
2016-04-27 22:07:58 +0200

    Initialisation

---
+++ tp5/src/main/java/fr/uga/tp5/App.java
@@ -0,0 +1,13 @@
+package fr.uga.tp5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Je m'appelle Frank" );
+    }
+}
